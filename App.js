beb98541c1fc7eb49d8a1951eb77524764597d48
maud08 <maudinette08@outlook.com>
2022-01-19 15:45:53 +0100

    exemple to react navigation

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, Text} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/components/HomeScreen';
+import DetailsScreen from './src/components/DetailsScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <>
-      <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
-      <View>
-        <Text>coucou</Text>
-      </View>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
